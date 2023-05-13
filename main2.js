80b9b06de2d16d77f5012bbc8b60a24d86fa940e
gitlinogy <98918808+gitlinogy@users.noreply.github.com>
2023-05-13 22:33:04 +0200

    Update and rename main2.js to main3.js

--- main2.js
+++
@@ -1,5 +0,0 @@
-import {test} from "./charts.js"
-
-console.log("export scripts don't work");
-
-test();
---
+++ main3.js
@@ -0,0 +1,5 @@
+import {renderGraphs} from './charts.js';
+
+console.log("export scripts don't work");
+
+renderGraphs();
